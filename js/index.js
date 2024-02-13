1f419e843c11c45b7d0adb7f7001798b41f30984
Orlando Landaeta <63929875+landaetadev@users.noreply.github.com>
2024-02-13 19:01:17 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,47 +1,36 @@
-var vCounter = false;
+var vShowVerticalMenu = false;
 var vBody = document.querySelector("body");
 let letIconsFile = "./assets/icons.svg#"
 
 //VARIABLES TOPBAR
-var vHeader = document.querySelector(".classHeader");
 var vBtnMenu = document.querySelector("#idBtnMenu");
 var vBtnsMenuOptions = document.querySelector(".classBtnsMenuOptions");
 var vBtnMenuOpt = document.querySelectorAll(".classBtnMenuOpt");
 
-//SHOW-HIDE TOPBAR
-vBody.onscroll = function () {
-	const letScrollYPosition = 300;
-	if (window.scrollY >= letScrollYPosition) {
-		vHeader.classList.add("classShowHeader");
+//SHOW-HIDE VERTICAL MENU
+vBtnMenu.addEventListener ("click", () => {
+	if (vShowVerticalMenu == false) {
+		funcJsShowMenuNav();
 	} else {
-		vHeader.classList.remove("classShowHeader");
+		funcJsHideMenuNav();
 	}
-};
+});
 
-	//SHOW-HIDE VERTICAL MENU
-	vBtnMenu.addEventListener ("click", () => {
-		if (vCounter == false) {
-			funcJsShowMenuNav();
-		} else {
-			funcJsHideMenuNav();
-		}
+//HIDE MENU WHEN CLICK
+for (let i = 0; i < vBtnMenuOpt.length; i++) {
+	vBtnMenuOpt[i].addEventListener("click", function() {
+		funcJsHideMenuNav();
 	});
-
-	//HIDE MENU WHEN CLICK
-	for (let i = 0; i < vBtnMenuOpt.length; i++) {
-		vBtnMenuOpt[i].addEventListener("click", function() {
-			funcJsHideMenuNav();
-		});
-	};
+};
 
 //SHOW-HIDE SLIDER
 function funcJsShowMenuNav() {
 	vBtnsMenuOptions.classList.add("classBtnsMenuOptionsShow");
 	vBody.classList.add("classHideOverflowY");
-	vCounter = true;
+	vShowVerticalMenu = true;
 };
 function funcJsHideMenuNav() {
 	vBtnsMenuOptions.classList.remove("classBtnsMenuOptionsShow");
 	vBody.classList.remove("classHideOverflowY");
-	vCounter = false;
+	vShowVerticalMenu = false;
 };

6e287c7e3bb33298e81fc9eeaadf78fc6ccbec1a
Orlando Landaeta <63929875+landaetadev@users.noreply.github.com>
2024-03-07 22:34:34 +0100

    Update

--- js/showSkills.js
+++ js/showSkills.js
@@ -1,6 +1,7 @@
 //SHOW SKILLS
 
 const vShowSkillCards = document.querySelector(".classHabilidadesFlex");
+let letIconsFile = "./assets/icons.svg#"
 var vMakeSkillCardHTML = ``;
 fetch('./js/JSONTableSkills.json')
 .then(response => response.json())
@@ -12,15 +13,15 @@ function ReadTableSkills(JSONDataSkills) {
 
 	for (var vJSONi = 0; vJSONi < JSONDataSkills.length; vJSONi++) {
 		//Make Project Card HTML
-	vMakeSkillCardHTML += `<div class="classHabilidad">`;
+		vMakeSkillCardHTML += `<div class="classHabilidad">`;
     vMakeSkillCardHTML += 	`<div class="classHabiIcono">`;
     vMakeSkillCardHTML += 		`<svg>`;
     vMakeSkillCardHTML += 			`<use href="${letIconsFile}${JSONDataSkills[vJSONi].jsonIcono}">`;
     vMakeSkillCardHTML += 		`</svg>`;
     vMakeSkillCardHTML += 	`</div>`;
-	vMakeSkillCardHTML += 	`<h3 class="classHabiNombre">${JSONDataSkills[vJSONi].jsonArea}</h3>`;
+		vMakeSkillCardHTML += 	`<h3 class="classHabiNombre">${JSONDataSkills[vJSONi].jsonArea}</h3>`;
     vMakeSkillCardHTML += 	`<p class="classHabiDetalles">${JSONDataSkills[vJSONi].jsonSoftwares}</p>`;
-	vMakeSkillCardHTML += `</div>`;
+		vMakeSkillCardHTML += `</div>`;
 	}
 	vShowSkillCards.innerHTML = vMakeSkillCardHTML;
 }

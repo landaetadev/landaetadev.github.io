a1d8afa3fd5a60b4866c8219183deec5a6ceb12a
Orlando Landaeta <63929875+landaetadev@users.noreply.github.com>
2026-03-20 23:53:33 +0100

    deploy astro to docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-const isGH = process.env.DEPLOY_ENV === 'GH_PAGES';
-
 export default defineConfig({
-  site: isGH
-    ? 'https://landaetadev.github.io'
-    : 'https://www.landaetadev.com',
+  site: 'https://www.landaetadev.com',
   base: '/',
-  build: {
-    // Forzar que los assets sean relativos
-    assetsPrefix: './',
-    inlineStyles: 'auto',
-  },
-  vite: {
-    build: {
-      target: 'esnext',
-    },
-  },
 });

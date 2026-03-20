537a21546bf3e74879913837017661bcc4a2df4d
Orlando Landaeta <63929875+landaetadev@users.noreply.github.com>
2026-03-20 21:41:51 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import { defineConfig } from 'astro/config';
 
-// Detecta si estamos desplegando a GH Pages
 const isGH = process.env.DEPLOY_ENV === 'GH_PAGES';
 
 export default defineConfig({
-  // URL usada en meta tags, RSS, etc.
   site: isGH
     ? 'https://landaetadev.github.io'
     : 'https://www.landaetadev.com',
-
-  // Ruta base para todos los assets (CSS, JS, imágenes)
   base: '/',
-
-  // Configuración de Vite para builds modernos
+  build: {
+    // Forzar que los assets sean relativos
+    assetsPrefix: './',
+    inlineStyles: 'auto',
+  },
   vite: {
     build: {
       target: 'esnext',

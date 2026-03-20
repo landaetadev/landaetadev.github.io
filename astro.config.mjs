754d4115479d06cee98413578a0cffdf75d90506
Orlando Landaeta <63929875+landaetadev@users.noreply.github.com>
2026-03-20 20:39:55 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://landaetadev.github.io"
+  site: 'https://www.landaetadev.com', // tu dominio personalizado
+  base: '/', // la raíz del dominio
 });

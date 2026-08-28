b2cd4de247e89c44fd190af1842e90ebde2df12f
Orlando Landaeta <63929875+landaetadev@users.noreply.github.com>
2026-08-28 22:31:55 +0200

    Add plugin supersvg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,21 @@
 import { defineConfig } from 'astro/config';
+import supersvgPlugin from 'vite-plugin-supersvg';
 //
 export default defineConfig({
   site: 'https://www.landaetadev.com',
   base: '/',
+  vite: {
+    plugins: [
+      supersvgPlugin({
+      srcDir: 'src/svgicons/',        // Carpeta de ficheros SVG
+      destDir: 'public/assets/icons/',       // Carpeta de sprites finales
+      config: {                       // Configuración de svg-sprite
+        shape: {
+          spacing: { padding: 2 },
+        },
+      },
+      lazy: true                // Sólo genera cuando ocurren cambios
+    })
+    ]
+  },
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
-export const vIconsFile = "/assets/images/icons.svg#";
+export const vIconsFolder = "/assets/icons/";
 export const vProjectsFolder = "/assets/images/projectsFiles/";
+
+export const vLinks = {
+  GitHub: "https://github.com/landaetadev",
+  LinkedIn: "https://www.linkedin.com/in/olandaeta/",
+  CV: "https://filedn.eu/lUfpa3BpLa45XCcSIQyWRHF/WWW/Portfolio/Orlando_Landaeta_CV.pdf"
+}

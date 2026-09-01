24ef3aa5fbd12165433f395c32ca3ece68cb3ab5
Orlando Landaeta <63929875+landaetadev@users.noreply.github.com>
2026-09-01 03:42:25 +0200

    minor fixes

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ export const vIconsFolder = "/assets/icons/";
 export const vProjectsFolder = "/assets/images/projectsFiles/";
 
 export const vLinks = {
-  GitHub: "https://github.com/landaetadev",
+  GitHub: "https://www.github.com/landaetadev",
   LinkedIn: "https://www.linkedin.com/in/olandaeta/",
   CV: "https://filedn.eu/lUfpa3BpLa45XCcSIQyWRHF/WWW/Portfolio/Orlando_Landaeta_CV.pdf"
 }
